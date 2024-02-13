240a7169e8e112d253eb0936f9b790c7b74efea0
ElmirDev <147965924+elmirfadil@users.noreply.github.com>
2024-02-13 19:10:17 +0600

    home (#1)

---
+++ src/pages/Home/HomeHero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div id="hero">
+      <div className="container">
+        <div className="hero">
+          <h1>Genres</h1>
+          <p>View all</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
